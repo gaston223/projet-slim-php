cbcc09d72bc341fbf77a36edbd92fd4c2736254b
gaston223 <g.coulibalyy@gmail.com>
2019-03-25 17:02:39 +0100

    Code Sniffer et routage dynamique

--- public/index.php
+++ public/index.php
@@ -1,3 +1,43 @@
 <?php
-echo 'Bonjour';
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+//echo 'Bonjour';
+
+$app = new App();
+
+$route = $app->get("/", function (
+    ServerRequestInterface $request,
+    ResponseInterface $response,
+    ?array $args
+) {
+
+    //on retourne une reponse
+
+    return $response->getBody()->write('<h1>Bonjour</h1>');
+});$route->setName('homepage');
+
+
+//routes groupées
+
+$app->group('/projet',function(){
+    // Route pour detail du projet
+    $this->get("/{id:\d+}",function (ServerRequestInterface $request, ResponseInterface $response, ?array $args
+        ) {
+            //on retourne une reponse
+            return $response->getBody()->write('<h1>Détail du projet</h1>');
+        })->setName('app_project_show');
+
+    $this->get("/creation",function (ServerRequestInterface $request, ResponseInterface $response, ?array $args
+    ) {
+        //on retourne une reponse
+        return $response->getBody()->write('<h1>Création du projet</h1>');
+    })->setName('app_project_create');
+});
+
+//Création et renvoi de la réponse au navigateur
+
+$app->run();
